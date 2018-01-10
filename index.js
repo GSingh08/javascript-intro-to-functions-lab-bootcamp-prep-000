bb9367c4267f9c1cb5f93f8a43de17021129f694
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-10 18:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ function whisper(string)
 
 function logShout(string)
 {
+<<<<<<< HEAD
   console.log(string.toUpperCase());
+=======
+  console.log(string.toUpperCase())
+>>>>>>> ca5f200c1bf9e5b743b064a96380d2b223c9186d
 }
 
 function logWhisper(string)
 {
+<<<<<<< HEAD
     console.log(string.toLowerCase());
   }
   
@@ -32,3 +37,21 @@ if(string.toUpperCase() === string) {
     return "I love you, too.";
   }
 }
+=======
+    console.log(string.toLowerCase())
+  }
+  
+  function sayHiToGrandma(string)
+  {
+  if (string.toLowerCase() === string) {
+     return "I can't hear you!"
+   }
+  }
+   if (string.toUpperCase() === string) {
+     return "YES INDEED!"
+   }
+ 
+   if (string === "I love you, Grandma.") {
+     return "I love you, too."
+   }
+>>>>>>> ca5f200c1bf9e5b743b064a96380d2b223c9186d
